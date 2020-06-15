c41deb966bfe66d015c9907fbe94f3b64289bf34
Jimmi Kardo Sitepu <32915206+jimmisitepu88@users.noreply.github.com>
2020-06-15 22:17:00 +0700

    Add files via upload

---
+++ Arduino-LiquidCrystal-I2C-library-master/examples/lcd_millis/lcd_millis.ino
@@ -0,0 +1,19 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(16, 2);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.autoAddress();
+  lcd.begin();
+  lcd.setCursor(0,0);
+  lcd.print("TEST LCD");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.setCursor(0,1);
+  lcd.print(millis()/1000);
+}
